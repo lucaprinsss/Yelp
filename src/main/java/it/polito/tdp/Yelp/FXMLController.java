81e0b001dde3921a6503af2e7bff28cd89ce3a3c
luca <luca@pc-Luca>
2022-04-15 16:41:14 +0200

    Ora funziona tutto

--- src/main/java/it/polito/tdp/Yelp/FXMLController.java
+++ src/main/java/it/polito/tdp/Yelp/FXMLController.java
@@ -2,24 +2,52 @@ package it.polito.tdp.Yelp;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.Yelp.model.Business;
+import it.polito.tdp.Yelp.model.Model;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class FXMLController implements Initializable {
-    
+public class FXMLController {
+	
+	private Model model ;
+
     @FXML
-    private Label label;
-    
+    private ResourceBundle resources;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private URL location;
+
+    @FXML
+    private TableColumn<Business, String> colName;
+
+    @FXML
+    private TableColumn<Business, Double> colStars;
+
+    @FXML
+    private TableView<Business> tblBusiness;
+
+    @FXML
+    void handleLoadData(ActionEvent event) {
+    	tblBusiness.setItems(FXCollections.observableArrayList(model.getBusinesses()));
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void setModel(Model m) {
+    	this.model = m ;
+    }
+
+    @FXML
+    void initialize() {
+        assert colName != null : "fx:id=\"colName\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert colStars != null : "fx:id=\"colStars\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert tblBusiness != null : "fx:id=\"tblBusiness\" was not injected: check your FXML file 'Scene.fxml'.";
+
+        colName.setCellValueFactory(new PropertyValueFactory<Business,String>("businessName"));
+        colStars.setCellValueFactory(new PropertyValueFactory<Business,Double>("stars"));
+    }
+
 }
