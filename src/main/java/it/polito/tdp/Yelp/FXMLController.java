543d666af352efcc472542228af5277782e2b9ce
luca <luca@pc-Luca>
2022-04-15 16:54:42 +0200

    priva

--- src/main/java/it/polito/tdp/Yelp/FXMLController.java
+++ src/main/java/it/polito/tdp/Yelp/FXMLController.java
@@ -12,7 +12,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-public class FXMLController {
+public class FXMLController { //finito
 	
 	private Model model ;
 
